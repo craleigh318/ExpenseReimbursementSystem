8d72300e4f7d7e84eb4f45bc53e9870c1bd41f7c
craleigh318 <craleigh318@comcast.net>
2019-01-27 18:42:53 -0500

    Update ErsDatabaseProcedures.java

--- src/com/revature/craleigh318/expense_reimbursement_system/dao/ErsDatabaseProcedures.java
+++ src/com/revature/craleigh318/expense_reimbursement_system/dao/ErsDatabaseProcedures.java
@@ -7,14 +7,14 @@ import java.sql.SQLException;
 
 public class ErsDatabaseProcedures {
 	
-	public CallableStatement insertEmployee(String username, String password) throws SQLException, IOException {
+	public static CallableStatement insertEmployee(String username, String password) throws SQLException, IOException {
 		CallableStatement stmt = getConnection().prepareCall("{CALL PROC_INSERT_EMPLOYEE(?,?)}");  
 		stmt.setString(1, username);  
 		stmt.setString(2, password);
 		return stmt;
 	}
 	
-	private Connection getConnection() throws SQLException, IOException {
+	private static Connection getConnection() throws SQLException, IOException {
 		return DataHandler.getInstance().getConnection();
 	}
 	
