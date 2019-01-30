91ce4dd28d14455623751e56eaed490bb9f17c74
craleigh318 <craleigh318@comcast.net>
2019-01-30 15:01:33 -0500

    Update Main.java

--- src/main/resources/com/revature/craleigh318/ers/Main.java
+++ src/main/resources/com/revature/craleigh318/ers/Main.java
@@ -1,9 +1,8 @@
 package com.revature.craleigh318.ers;
 
-import com.revature.craleigh318.ers.app.employee_registration.EmployeeRegistrationController;
+public class Main {
 
-public class Main extends EmployeeRegistrationController {
-
-	private static final long serialVersionUID = -4568788731609069582L;
-	
+	public static void main(String args[]) {
+		System.out.println("Hello world");
+	}
 }
