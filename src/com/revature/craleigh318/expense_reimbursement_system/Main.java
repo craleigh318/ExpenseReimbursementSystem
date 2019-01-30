e48877193e3ae31fcbbb5a23dbe24b31f2124fdc
craleigh318 <craleigh318@comcast.net>
2019-01-29 22:43:40 -0500

    Selected new main class.

--- src/com/revature/craleigh318/expense_reimbursement_system/Main.java
+++ src/com/revature/craleigh318/expense_reimbursement_system/Main.java
@@ -1,15 +1,9 @@
 package com.revature.craleigh318.expense_reimbursement_system;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.revature.craleigh318.expense_reimbursement_system.app.employee_registration.EmployeeRegistrationController;
 
 public class Main extends EmployeeRegistrationController {
+
+	private static final long serialVersionUID = -4568788731609069582L;
 	
 }
