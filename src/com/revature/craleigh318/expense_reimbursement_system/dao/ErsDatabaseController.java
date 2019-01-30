61b0718fe5c0940d9afe8e49f4f81610717cc53c
craleigh318 <craleigh318@comcast.net>
2019-01-29 21:04:55 -0500

    Update ErsDatabaseController.java

--- src/com/revature/craleigh318/expense_reimbursement_system/dao/ErsDatabaseController.java
+++ src/com/revature/craleigh318/expense_reimbursement_system/dao/ErsDatabaseController.java
@@ -3,11 +3,11 @@ package com.revature.craleigh318.expense_reimbursement_system.dao;
 import java.io.IOException;
 import java.sql.SQLException;
 
-public class ErsRestController {
+public class ErsDatabaseController {
 
 	public void userRegister(String username, String temporaryPassword) throws SQLException, IOException {
 		ErsDatabaseProcedures.insertEmployee(username, temporaryPassword).execute();
 	}
 	
-	private ErsRestController() { }
+	private ErsDatabaseController() { }
 }
