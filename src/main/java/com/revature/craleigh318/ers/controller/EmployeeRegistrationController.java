a2dca36e459aa25f05f514a6656cbb6def8afbda
craleigh318 <craleigh318@comcast.net>
2019-02-05 15:32:19 -0500

    Added registration attribute names.

--- src/main/java/com/revature/craleigh318/ers/controller/EmployeeRegistrationController.java
+++ src/main/java/com/revature/craleigh318/ers/controller/EmployeeRegistrationController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.revature.craleigh318.ers.data.ErsDao;
+import com.revature.craleigh318.ers.utils.AttributeNames;
 import com.revature.craleigh318.ers.utils.FormResponse;
 import com.revature.craleigh318.ers.view.EmployeeRegistrationView;
 import com.revature.craleigh318.ers.view.ErsView;
@@ -28,8 +29,8 @@ public class EmployeeRegistrationController implements IErsViewController {
 	}
 	
 	private FormResponse registerUser(HttpServletRequest req) {
-		String username = req.getParameter("registrationUsername");
-		String password = req.getParameter("registrationPassword");
+		String username = req.getParameter(AttributeNames.RGSTR_USERNAME);
+		String password = req.getParameter(AttributeNames.RGSTR_PASSWORD);
 		if ((username == null) && (password == null)) {
 			return new FormResponse(FormResponse.Code.NOT_SUBMITTED, null);
 		}
--- src/main/java/com/revature/craleigh318/ers/utils/AttributeNames.java
+++ src/main/java/com/revature/craleigh318/ers/utils/AttributeNames.java
@@ -6,6 +6,8 @@ public class AttributeNames {
 	public static final String USER_ID = "user-id";
 	public static final String LOGIN_USERNAME = "loginUsername";
 	public static final String LOGIN_PASSWORD = "loginPassword";
+	public static final String RGSTR_USERNAME = "registrationUsername";
+	public static final String RGSTR_PASSWORD = "registrationPassword";
 
 	private AttributeNames() { }
 }
