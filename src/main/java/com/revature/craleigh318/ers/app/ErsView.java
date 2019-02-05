ed726023d365c7b4ea6564dfb637af171ae86497
craleigh318 <craleigh318@comcast.net>
2019-02-05 13:29:11 -0500

    Update ErsView.java

--- src/main/java/com/revature/craleigh318/ers/app/ErsView.java
+++ src/main/java/com/revature/craleigh318/ers/app/ErsView.java
@@ -6,6 +6,12 @@ import java.io.PrintWriter;
 import javax.servlet.ServletResponse;
 
 public class ErsView {
+	
+	private static final String OUTER_HTML = "<!DOCTYPE html><html><head><title>Expense Reimbursement System</title></head><body>%s</body></html>";
+
+	public static String addOuterHtml(String innerHtml) {
+		return String.format(OUTER_HTML, innerHtml);
+	}
 
 	public static void writeHtml(ServletResponse response, String html) throws IOException {
 		response.setContentType("text/html");
