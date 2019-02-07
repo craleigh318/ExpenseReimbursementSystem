8ddf2d1e6cb0f6f4ef3573836713db6c63c84672
craleigh318 <craleigh318@comcast.net>
2019-02-07 12:54:41 -0500

    Update ReimbursementTableView.java

--- src/main/java/com/revature/craleigh318/ers/view/ReimbursementTableView.java
+++ src/main/java/com/revature/craleigh318/ers/view/ReimbursementTableView.java
@@ -1,25 +1,21 @@
 package com.revature.craleigh318.ers.view;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.util.Date;
-import java.text.SimpleDateFormat;
 
 import com.revature.craleigh318.ers.model.ReimbursementRequest;
 
 public class ReimbursementTableView {
 
 	private static final String TH_EMPLOYEE = "<th>Employee ID</th>";
-	private static final String TABLE_HEADERS = "<tr>%s<th>Amount ($)</th><th>Date</th><th>Description</th><th>Status</th></tr>";
+	private static final String TABLE_HEADERS = "<tr>%s<th>Amount</th><th>Date</th><th>Description</th><th>Status</th></tr>";
 	private static final String TABLE_HEADERS_EMPLOYEE = String.format(TABLE_HEADERS, "");
 	private static final String TABLE_HEADERS_MANAGER = String.format(TABLE_HEADERS, TH_EMPLOYEE);
 	private static final String TABLE_CELL = "<td>%s</td>";
 	private static final String TABLE_TEMPLATE = "<table>%s</table>";
 	private static final String ROW_TEMPLATE = "<tr>%s</tr>";
-	private static final int EMPLOYEE_TABLE_ROW_LENGTH = 3;
-	private static final int MANAGER_TABLE_ROW_LENGTH = EMPLOYEE_TABLE_ROW_LENGTH + 2;
-	
-	//private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-	
+
 	public static String createTable(Iterable<ReimbursementRequest> requests, boolean manager) {
 		String headers;
 		if (manager) {
@@ -53,7 +49,8 @@ public class ReimbursementTableView {
 	
 	private static String createEmployeeRow(ReimbursementRequest request) {
 		BigDecimal amount = request.getAmount();
-		String tdAmount = String.format(TABLE_CELL, amount.toString());
+		String strAmount = NumberFormat.getCurrencyInstance().format(amount);
+		String tdAmount = String.format(TABLE_CELL, strAmount);
 		StringBuilder sb = new StringBuilder(tdAmount);
 		Date date = request.getRequestDate();
 		String tdDate = String.format(TABLE_CELL, date.toString());
